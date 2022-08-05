afacd90001161a8402a4737043d9e932fd082105
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-05 16:22:39 -0600

    feat: Completing Using the Console exercises

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // Log to console
-console.log
+console.log('Bon jour le monde!');
+console.log(123);
+console.log(true);
+var greeting = "Priviet!"
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is an error!');
+console.clear();
+console.warn('This is a warning!');
+console.time('Hello');
+  console.log('Hello world!')
+  console.log('Hello world!')
+  console.log('Hello world!')
+  console.log('Hello world!')
+  console.log('Hello world!')
+console.timeEnd('Hello');
