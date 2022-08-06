0b4c8cf3a9f67a7a6487b7d338846ac9d228737d
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-06 11:12:55 -0600

    feat: Completed data typ exercise

--- app.js
+++ app.js
@@ -1,56 +1,94 @@
-// Log to console
-console.log('Bon jour le monde!');
-console.log(123);
-console.log(true);
-var greeting = "Priviet!"
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
+// // Log to console
+// console.log('Bon jour le monde!');
+// console.log(123);
+// console.log(true);
+// var greeting = "Priviet!"
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
 
-console.error('This is an error!');
-console.clear();
-console.warn('This is a warning!');
-console.time('Hello');
-  console.log('Hello world!')
-  console.log('Hello world!')
-  console.log('Hello world!')
-  console.log('Hello world!')
-  console.log('Hello world!')
-console.timeEnd('Hello');
+// console.error('This is an error!');
+// console.clear();
+// console.warn('This is a warning!');
+// console.time('Hello');
+//   console.log('Hello world!')
+//   console.log('Hello world!')
+//   console.log('Hello world!')
+//   console.log('Hello world!')
+//   console.log('Hello world!')
+// console.timeEnd('Hello');
 
-// var name = 'Nurse Chapel';
+// // var name = 'Nurse Chapel';
+// // console.log(name);
+
+// // console.log(name);
+
+// var firstName = 'Seven';
+// var lastName = 'Of Nine';
+// console.log(firstName + ' ' + lastName);
+
+// // LET
+// // let name1;
+// // name1 = 'T\'Pol Vulcanness';
+// // console.log(name1);
+// // name1 = 'Major';
+// // console.log(name1);
+
+// // CONST 
+// const name = 'Alita';
 // console.log(name);
+// const greeting1 = 'Ahlan wa Sahlan';
+// console.log(greeting1);
+// const person = {
+//   name: 'Beckett',
+//   age: '24'
+// }
 
-// console.log(name);
+// console.log(person);
 
-var firstName = 'Seven';
-var lastName = 'Of Nine';
-console.log(firstName + ' ' + lastName);
+// person.name = 'Sara';
 
-// LET
-// let name1;
-// name1 = 'T\'Pol Vulcanness';
-// console.log(name1);
-// name1 = 'Major';
-// console.log(name1);
+// console.log(person);
 
-// CONST 
-const name = 'Alita';
-console.log(name);
-const greeting1 = 'Ahlan wa Sahlan';
-console.log(greeting1);
-const person = {
-  name: 'Beckett',
-  age: '24'
+// const numbers = [1,2,3,4,5];
+// numbers.push(6);
+// console.log(numbers);
+
+// PRIMITIVE
+
+// String
+const name = 'Seven of Nine';
+// Number
+const age = 19;
+// Boolean
+const hasKids = false;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES
+// Array
+const hobbies = ['movies', 'fitness'];
+// Object literal 
+const address =  {
+  city: 'Puerto Galera',
+  state: 'Phillipines'
 }
 
-console.log(person);
+// Dates 
+const today = new Date();
+console.log(today);
+console.log(typeof address);
 
-person.name = 'Sara';
 
-console.log(person);
 
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-console.log(numbers);
+
+
+
+
+
+
