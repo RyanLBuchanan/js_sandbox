3e3b09a1000068d0f5fdaeacf761624ca2f8f320
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-06 11:53:19 -0600

    feat: Completed strings and concatenation

--- app.js
+++ app.js
@@ -58,37 +58,131 @@
 // PRIMITIVE
 
 // String
-const name = 'Seven of Nine';
-// Number
-const age = 19;
-// Boolean
-const hasKids = false;
-// Null
-const car = null;
-// Undefined
-let test;
-// Symbol
-const sym = Symbol();
+// const name = 'Seven of Nine';
+// // Number
+// const age = 19;
+// // Boolean
+// const hasKids = false;
+// // Null
+// const car = null;
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol();
 
-// REFERENCE TYPES
-// Array
-const hobbies = ['movies', 'fitness'];
-// Object literal 
-const address =  {
-  city: 'Puerto Galera',
-  state: 'Phillipines'
-}
+// // REFERENCE TYPES
+// // Array
+// const hobbies = ['movies', 'fitness'];
+// // Object literal 
+// const address =  {
+//   city: 'Puerto Galera',
+//   state: 'Phillipines'
+// }
 
-// Dates 
-const today = new Date();
-console.log(today);
-console.log(typeof address);
+// // Dates 
+// const today = new Date();
+// console.log(today);
+// console.log(typeof address);
+
+// let val;
+
+// // Number to string
+// val = String(42);
+// val = String(4+4);
+
+// // Boolean to string
+// val = String(true);
+
+// // Date to string
+// val = String(new Date());
+
+// // Array to string 
+// val  = String([1,2,3,4]);
+
+// // toString()
+// val = (5).toString();
+
+// val = (true).toString();
+
+// val = Number('5');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('Hello');
+// val = Number([1,2,3]);
+
+// val = parseFloat('100.30');
+
+// // Output
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed(2));
+
+// const val1 = String(5);
+// const val2 = 6;
+// const sum = val1 + val2;
+
+// console.log(sum);
+// console.log(typeof sum);
+// const num1 = 100;
+// const num2 = 50;
+// let val;
+
+// // Simple math with numbers
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// // Math Object
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.4);
+// val = Math.ceil(2.4);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.max(2,33,3,4,-56,1);
+// val = Math.random();  // Decimal
+
+// val = Math.random() * 20;
+// // val = Math.floor(Math.random() * 20);
+
+// console.log(val);
+// console.log(Math.round(val));
+
+// Append
+
+firstName = 'Seven';
+lastName = 'Of Nine';
+
+let val;
+
+val = 'Seven ';
+val += 'Of Nine';
 
 
+// Escaping
+val = "T'Pol Vulcanness";
+val = 'T\'Pol Vulcanness\' cat';
+
+val = firstName.concat(' ',  lastName);
+
+val = firstName[0];
+
+val = firstName.indexOf('v');
 
 
+const str = 'Hello my name is T\'Pol';
+val = str.split(' ');
+val = str.replace('T\'Pol', 'Seven of Nine');
 
+val = str.includes('foo');
 
+console.log(val);
 
 
 
