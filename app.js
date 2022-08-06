199b76ea4e54eb75fa693c0601669ab3d5fa12fa
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-06 10:58:23 -0600

    feat: Completed declaring variables exercise

--- app.js
+++ app.js
@@ -19,7 +19,38 @@ console.time('Hello');
   console.log('Hello world!')
 console.timeEnd('Hello');
 
-var name = 'Nurse Chapel';
-console.log(name);
+// var name = 'Nurse Chapel';
+// console.log(name);
 
+// console.log(name);
+
+var firstName = 'Seven';
+var lastName = 'Of Nine';
+console.log(firstName + ' ' + lastName);
+
+// LET
+// let name1;
+// name1 = 'T\'Pol Vulcanness';
+// console.log(name1);
+// name1 = 'Major';
+// console.log(name1);
+
+// CONST 
+const name = 'Alita';
 console.log(name);
+const greeting1 = 'Ahlan wa Sahlan';
+console.log(greeting1);
+const person = {
+  name: 'Beckett',
+  age: '24'
+}
+
+console.log(person);
+
+person.name = 'Sara';
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
