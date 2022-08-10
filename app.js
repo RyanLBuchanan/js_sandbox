99ad36727594483b6b404c3fe9885398f43584fa
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-09 21:36:35 -0600

    feat: Completed Keyboard and input events

--- app.js
+++ app.js
@@ -387,4 +387,35 @@
   // console.log(taskInput.value);
 
   // e.preventDefault();
+// }
+
+// EVENT BUBBLING
+// document.querySelector('.card-title').addEventListener('click',
+// function(){
+//   console.log('card-title');
+// }); 
+
+// document.querySelector('.card-content').addEventListener('click',
+// function(){
+//   console.log('card-content');
+// }); 
+
+// document.querySelector('.card').addEventListener('click',
+// function(){
+//   console.log('card');
+// }); 
+
+// document.querySelector('.col').addEventListener('click',
+// function(){
+//   console.log('col');
+// }); 
+
+
+// EVENT DELEGATION event on parent and going down opposite of bubbling
+const delItem = document.querySelector('.delete-item');
+
+delItem.addEventListener('click', deleteItem);
+
+function deleteItem(){
+  console.log('delete item');
 }
